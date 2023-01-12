797076c69db7c8c8814b75ba14c2297843399ce5
shkisme <shkisme0130@gmail.com>
2023-01-13 00:50:28 +0900

    add: 3주차 java 코드 업로드

--- shkisme/java/Transaction.java
+++ shkisme/java/Transaction.java
@@ -1,14 +1,33 @@
 import java.util.Currency;
 
 public class Transaction {
-    private Currency currency;
-    private int price;
 
-    public Currency getCurrency() {
-        return currency;
-    }
+  private final Trader trader;
 
-    public int getPrice() {
-        return price;
-    }
+  private final int value;
+  private final int year;
+
+  private Currency currency;
+
+  public Transaction(Trader trader, int year, int value) {
+    this.trader = trader;
+    this.value = value;
+    this.year = year;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public Trader getTrader() {
+    return trader;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public Currency getCurrency() {
+    return currency;
+  }
 }
